9a4e91e0e090873fd2ce543adf776fa803310ab2
Andrew Fiasco <affiasco@umich.edu>
2023-05-29 00:07:59 -0400

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (n1, n2) {
+  if (n1 < 0 || n2 < 0) return "ERROR";
+  if (typeof n1 !== "number" || typeof n2 !== "number") return "ERROR";
+  let count = 0;
+  let small;
+  let large;
 
+  if (n1 < n2) {
+    small = n1;
+    large = n2;
+  } else {
+    small = n2;
+    large = n1;
+  }
+
+  for (let i = small; i <= large; i++) {
+    count += i;
+  }
+  return count;
 };
 
 // Do not edit below this line
